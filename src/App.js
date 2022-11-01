51b4ca3ab7e0c48a8271417129cd667f672ac2d7
Rahul12wd <96861219+Rahul12wd@users.noreply.github.com>
2022-11-01 17:29:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Resume from './components/resume'
+
+function App() {
+  return (
+    <div className="App">
+     <Resume/>
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/resume.js
@@ -0,0 +1,55 @@
+function Resume (){
+    return(
+        <>
+        <h1>Rahul Phad</h1>
+        <p>Software Developer</p>
+        <hr />
+
+        <div className='contact'>
+             <p>Email :  rahulphad225@gamil.com</p> 
+             <p>Phone No. :  +91-9146716564</p> 
+             <p><a href="https://github.com/RahulPhad12">Github</a></p>
+             <p><a href="www.linkedin.com/in/rahul-phad">Linkdin</a></p> 
+        </div>
+       
+
+
+        <h2>Experience</h2>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic minima voluptatem quia cupiditate? Pariatur doloribus
+            voluptas deleniti esse quam soluta ratione sed culpa aspernatur id, ut incidunt aute qui deserunt!</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, odio quae cum deserunt pariatur possimus! Obcaecati atque inventore
+            maxime ab cum ipsam eligendi explicabo tempora exercitationem incidunt, quae quam vel.</p>    
+        <hr />
+
+        <h2>Projects</h2>
+       <h4>Github Account Finder  [Live]  [Github Repo]</h4>
+       <ul>
+         <li> It is a Github account finding app where we can find users .</li>
+         <li>   It includes features like we can check repositories, following and follower list.</li>
+         <li>   Tech stack used : React</li>
+            </ul>
+        <hr />
+
+        <h2> Tech Skills</h2>   
+        <ul>
+            <li>Java</li>
+            <li>Data Structure and Algorithm</li>
+            <li>Javascript</li>
+            <li>React</li>
+        </ul> 
+        <hr />
+
+        <h2>Hobbies</h2>
+        <ol>
+            <li>Cricket</li>
+            <li>Travel</li>
+            <li>Reading Books</li>
+            <li>Driving</li>
+        </ol>
+        </>
+
+    )
+}
+
+
+export default Resume;
